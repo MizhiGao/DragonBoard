3f7bb0e5e0fa37ec1d689278db9160753d3ef113
xycldxwb <mimi_gao@qq.com>
2021-03-11 10:38:02 +0800

    use Jquery animate method to relize the function of carousel

--- src/dragonJQuery.js
+++ src/dragonJQuery.js
@@ -9,7 +9,7 @@ $(function() {
             itemArr: [],
             defaultItemLength: null,
             itemWidth: null,
-            translateX: null,
+            marginLeft: null,
             duration: 500,
             index: 0,
             isLock: false,
@@ -72,72 +72,43 @@ $(function() {
             PAGE.data.itemWidth = $('.teacher-item').width();
             let index = PAGE.data.index ;
             let itemWidth = PAGE.data.itemWidth;   
-            PAGE.data.translateX = - itemWidth * (index + 4);  
+            PAGE.data.marginLeft = - itemWidth * (index + 4);  
             PAGE.data.itemArr = $('.teacher-item').clone();
             let teacherItem = PAGE.data.itemArr;
             $('#teacher-list').append($('.teacher-item').clone().slice(0, PAGE.data.defaultItemLength - 1)).prepend($(teacherItem).slice(1));
-            $('#teacher-list').attr('style',`transform:translateX(${PAGE.data.translateX}px)`);
-            PAGE.goIndex(index);
+            $('#teacher-list').attr('style',`margin-left:${PAGE.data.marginLeft}px`);
         },
         goIndex: function(index) {
             let duration = PAGE.data.duration;
             let itemWidth = PAGE.data.itemWidth;          
-            let beginTranslateX = PAGE.data.translateX ;
-            let endTranslateX = - itemWidth * (index + 4);
             let isLock =PAGE.data.isLock;
             if(isLock){
                 return
             }
-            PAGE.data.isLock = true;
-            /*$('#teacher-list').attr('style','');
-            $('#teacher-list').animate({},function() {
-                $('#teacher-list').attr('style',`transform:translateX(${endTranslateX}px)`)
-            });*/
-            
-            PAGE.data.index = index;
-            PAGE.data.translateX = endTranslateX;
-            PAGE.data.isLock = false;
-            PAGE.animateTo(beginTranslateX, endTranslateX, duration, function(value) {
-                $('#teacher-list').attr('style', `transform:translateX(${value}px)`);
-            },function(value) {  
-                let translateX = value;
-                $('#teacher-list').attr('style',`transform:translateX(${translateX}px)`);
+            PAGE.data.isLock = true;      
+            PAGE.data.index = index;           
+            $('#teacher-list').animate({'margin-left': '-='+itemWidth}, duration, function(){
                 if(index === -4){
                     index = 1;
-                    value= - itemWidth * (index + 4);  
+                    PAGE.data.index = index;
+                    $('#teacher-list').css('margin-left', itemWidth * (index + 4));  
                 }
                 if(index === 5){
                     index = 0;
-                    value= - itemWidth * (index + 4); 
+                    PAGE.data.index = index;
+                    $('#teacher-list').css('margin-left', -itemWidth * (index + 4)); 
                 }
-                PAGE.data.index = index;
-                PAGE.data.translateX = value;
                 PAGE.data.isLock = false;
-            }) 
-        },
-        animateTo: function(beigin, end, duration, changeCallback, finishCallback){
-            let startTime = Date.now();
-            requestAnimationFrame(function update() {
-                let dateNow = Date.now();
-                let time = dateNow - startTime;
-                let value = PAGE.linear(beigin, end, time, duration); 
-                typeof changeCallback === 'function' && changeCallback(value);
-                if(startTime + duration > dateNow){
-                    requestAnimationFrame(update);
-                }else{
-                    typeof finishCallback === 'function' && finishCallback(end);
-                }
-            })  
-        },
-        linear: function(begin, end, time, duration) {       
-            return value = (end - begin)/duration * time + begin;
+            })
         },
         arrowPrev: function() {
-            let index = PAGE.data.index;    
+            let index = PAGE.data.index;
+            PAGE.data.itemWidth = - $('.teacher-item').width();   
             PAGE.goIndex(index - 1);
         },
         arrowNext: function() {
             let index = PAGE.data.index;
+            PAGE.data.itemWidth =  $('.teacher-item').width()
             PAGE.goIndex(index + 1);
         },
         hideLesson: function(e) {
